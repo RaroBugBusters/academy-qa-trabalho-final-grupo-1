3f50cde9e5d5ffbfffabeabb6b807704efece129
Patrícia Costa <patiii.costa@gmail.com>
2024-06-10 19:56:40 -0300

    Configura teste

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,2 @@
-import "./avaliacaoCommands";
 import "./filmesCommands";
 import "./usuarioCommands";
--- cypress/support/filmesCommands.js
+++ cypress/support/filmesCommands.js
@@ -1,48 +1,18 @@
 import { faker } from "@faker-js/faker";
 
-Cypress.Commands.add("criaMockFilme", () => ({
-  title: faker.lorem.words(5),
-  genre: faker.lorem.words(5),
-  description: faker.lorem.sentence(),
-  durationInMinutes: faker.number.int(140),
-  releaseYear: faker.date.past().getFullYear(),
-}));
-
 Cypress.Commands.add("criaFilme", () => {
-  cy.criaMockFilme().then((filme) => {
-    cy.request({
-      method: "POST",
-      url: "/movies",
-      body: filme,
-      headers: {
-        Authorization: `Bearer ${Cypress.env("accessToken")}`,
-      },
-    }).then(({ body: filmeCriado }) => {
-      Cypress.env("filmeAtual", filmeCriado);
-      return filmeCriado;
-    });
-  });
-});
-
-Cypress.Commands.add("deletaFilme", () => {
-  const { id: filmeId } = Cypress.env("filmeAtual");
-
-  if (filmeId) {
-    cy.request({
-      method: "DELETE",
-      url: `/movies/${filmeId}`,
-      headers: {
-        Authorization: `Bearer ${Cypress.env("accessToken")}`,
-      },
-    }).then(() => {
-      Cypress.env("filmeAtual", null);
-    });
-  }
+  return {
+    title: faker.lorem.words(5),
+    genre: faker.lorem.words(5),
+    description: faker.lorem.sentence(),
+    durationInMinutes: faker.number.int(140),
+    releaseYear: faker.date.past().getFullYear(),
+  };
 });
 
 Cypress.Commands.add("criaERecuperaFilme", () => {
   cy.logaUsuarioAdmin().then(() => {
-    cy.criaMockFilme().then((filme) => {
+    cy.criaFilme().then((filme) => {
       cy.request({
         method: "POST",
         url: "/movies",
@@ -53,10 +23,9 @@ Cypress.Commands.add("criaERecuperaFilme", () => {
       }).then(() => {
         cy.request("GET", `/movies/search?title=${filme.title}`).then(
           ({ body }) => {
-            const filmeCriado = body[0];
-            Cypress.env("filmeAtual", filmeCriado);
-            return filmeCriado;
-          },
+            Cypress.env("filmeAtual", body[0]);
+            return body[0];
+          }
         );
       });
     });
