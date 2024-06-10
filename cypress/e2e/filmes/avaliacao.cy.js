73376cf02925047e2c558a79bdcbb9a82ab22578
Patrícia Costa <patiii.costa@gmail.com>
2024-06-10 19:53:24 -0300

    Teste de avaliação

---
+++ cypress/e2e/filmes/avaliacao.cy.js
@@ -0,0 +1,28 @@
+describe("Avaliação por usuários autenticados", () => {
+    it("Deve permitir que apenas usuários autenticados escrevam uma avaliação sobre um filme", () => {});
+  });
+  
+  describe("Seleção de nota e texto na avaliação", () => {
+    it("Deve permitir que o usuário selecione uma nota e informe um texto ao avaliar um filme", () => {});
+  });
+  
+  describe("Nota obrigatória, texto opcional", () => {
+    it("Deve exigir que o usuário selecione uma nota, mas permitir que o texto seja opcional", () => {});
+  });
+  
+  describe("Detalhes são refletidos imediatamente no filme avaliado", () => {
+    it("Deve atualizar os detalhes do filme imediatamente após a avaliação ser escrita", () => {});
+  });
+  
+  describe("Usuário pode refazer uma avaliação e que a nova avaliação atualiza a anterior", () => {
+    it("Deve atualizar a avaliação existente ao invés de criar uma nova", () => {});
+  });
+  
+  describe("Avaliação deve ter até 500 caracteres", () => {
+    it("Deve limitar o texto da avaliação a no máximo 500 caracteres", () => {});
+  });
+  
+  describe("A nota para um filme deve ser um valor inteiro entre 1 e 5.", () => {
+    it("Deve permitir que a nota da avaliação seja um valor inteiro entre 1 e 5", () => {});
+  });
+  
