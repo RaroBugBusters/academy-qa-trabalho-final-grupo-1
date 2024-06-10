666bf0107eb41fc8a7ce671d24c81a10d84f6288
Gustavo Augusto Borges de Lima <g.lima91@hotmail.com>
2024-06-10 16:43:14 -0300

    Ampliação dos testes de cadastro

--- cypress/e2e/features/userRegistration.feature
+++ cypress/e2e/features/userRegistration.feature
@@ -12,7 +12,7 @@ Cenario: Cadastro de usuário com campo email invalido
 
     Dado que eu acesso o site
     E quero registrar um usuário
-    Quando eu informar o dado de <nome>, <email> invalido, <senha> e <confirmarSenha>
+    Quando eu informar os dados de <nome>, <email> invalido, <senha> e <confirmarSenha>
     Então uma mensagem de erro deve ser exibida
     Exemplos:
     | nome         | email              | senha    | confirmarSenha  |
@@ -20,3 +20,30 @@ Cenario: Cadastro de usuário com campo email invalido
     | "BugBusters" | "bugbusters.com"   | "123456" | "123456"        |
     | "BugBusters" | "bugbusters@qa"    | "123456" | "123456"        |
     | "BugBusters" | "@qa.com"          | "123456" | "123456"        |
+
+Cenario: Cadastro de usuário com campo senha e confirmar senha diferentes
+
+    Dado que eu acesso o site
+    E quero registrar um usuário
+    Quando eu informar os dados com senha e confirmar senha diferentes
+    Então uma mensagem de senhas diferentes deve ser exibida
+
+Cenario: Cadastro de usuário com email ja existente
+
+    Dado que eu acesso o site
+    E quero registrar um usuário
+    Quando eu informar os dados de email ja existente
+    Então uma mensagem de email ja existente deve ser exibida
+
+Cenario: Cadastro de usuário com campos obrigatórios vazios
+
+    Dado que eu acesso o site
+    E quero registrar um usuário
+    Quando eu informar os dados de <Nome>, <Email>, <Senha> e <ConfirmarSenha> vazios
+    Então uma mensagem de preenchimento obrigatório deve ser exibida
+    Exemplos:
+    | Nome         | Email                | Senha       | ConfirmarSenha   |
+    | ""           | "bugbusters@qa.com"  | "123456"    | "123456"         |
+    | "BugBusters" | ""                   | "123456"    | "123456"         |
+    | "BugBusters" | "bugbusters@qa.com"  | ""          | "123456"         |
+    | "BugBusters" | "bugbusters@qa.com"  | "123456"    | ""               |
--- cypress/support/steps/userRegistration.step.js
+++ cypress/support/steps/userRegistration.step.js
@@ -3,6 +3,7 @@ import UserRegistrationPage from "../pages/UserRegistration.page";
 import { faker } from "@faker-js/faker";
 
 const userRegistrationPage = new UserRegistrationPage();
+var mail = faker.internet.email();
 
 Before(() => {
   cy.viewport("macbook-16");
@@ -19,13 +20,13 @@ When("quero registrar um usuário", () => {
 When("eu informar os dados de cadastro corretamente", () => {
 
   userRegistrationPage.typeName(faker.person.fullName())
-  userRegistrationPage.typeEmail(faker.internet.email())
+  userRegistrationPage.typeEmail(Email)
   userRegistrationPage.typePassword("123456")
   userRegistrationPage.typeConfirmPassword("123456")
   userRegistrationPage.Submit();
 });
 
-When("eu informar o dado de {string}, {string} invalido, {string} e {string}", (nome, email, senha, confirmarSenha) => {
+When("eu informar os dados de {string}, {string} invalido, {string} e {string}", (nome, email, senha, confirmarSenha) => {
 
   userRegistrationPage.typeName(nome)
   userRegistrationPage.typeEmail(email)
@@ -34,15 +35,55 @@ When("eu informar o dado de {string}, {string} invalido, {string} e {string}", (
   userRegistrationPage.Submit();
 });
 
+When("eu informar os dados com senha e confirmar senha diferentes", () => {
+
+  userRegistrationPage.typeName(faker.person.fullName())
+  userRegistrationPage.typeEmail(faker.internet.email())
+  userRegistrationPage.typePassword("123456")
+  userRegistrationPage.typeConfirmPassword("1234567")
+  userRegistrationPage.Submit();
+});
+
+When("eu informar os dados de email ja existente", () => {
+
+  userRegistrationPage.typeName(faker.person.fullName())
+  userRegistrationPage.typeEmail(mail)
+  userRegistrationPage.typePassword("123456")
+  userRegistrationPage.typeConfirmPassword("123456")
+  userRegistrationPage.Submit();
+});
+
+When("eu informar os dados de <Nome>, <Email>, <Senha> e <ConfirmarSenha> vazios", (Nome, Email, Senha, ConfirmarSenha) => {
+
+  userRegistrationPage.typeName(Nome)
+  userRegistrationPage.typeEmail(Email)
+  userRegistrationPage.typePassword(Senha)
+  userRegistrationPage.typeConfirmPassword(ConfirmarSenha)
+  userRegistrationPage.Submit();
+});
+
 Then("uma mensagem de sucesso deve ser exibida", () => {
 
   cy.get(".error-message").should("contain", "Cadastro realizado!");
   cy.get("div.modal-actions > button").click();
-
 });
 
 Then("uma mensagem de erro deve ser exibida", () => {
 
   cy.get(".input-error").should("contain", "Informe um e-mail válido.");
+});
 
+Then("uma mensagem de senhas diferentes deve ser exibida", () => {
+
+  cy.get(".input-error").should("contain", "As senhas devem ser iguais.");
+});
+
+Then("uma mensagem de email ja existente deve ser exibida", () => {
+
+  cy.get(".input-error").should("contain", "E-mail ja existente.");
+});
+
+Then("uma mensagem de preenchimento obrigatório deve ser exibida", () => {
+
+  cy.get(".input-error").should("be.visible");
 });
