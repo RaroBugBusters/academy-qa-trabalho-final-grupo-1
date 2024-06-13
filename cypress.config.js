467a0f476e811b7c047df380d5b4add0091aac38
Jey <andre.marqquez@gmail.com>
2024-06-12 21:34:47 -0300

    add: env API_URL

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,9 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://raromdb-frontend-c7d7dc3305a0.herokuapp.com/",
+    env: {
+      API_URL: "https://raromdb-3c39614e42d4.herokuapp.com/api",
+    },
     specPattern: "cypress/e2e/**/*.feature",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
