7bef6062f58d6864da9cf1cfa49e42576570b349
Jey <andre.marqquez@gmail.com>
2024-06-13 22:20:11 -0300

    add: page e step usuario

---
+++ cypress/support/pages/loginUsuarioPage.js
@@ -0,0 +1,17 @@
+export class LoginUsuario {
+  inputEmail = '[name="email"]';
+  inputSenha = '[name="password"]';
+  buttonLogin = ".login-button";
+
+  clicarLogin() {
+    cy.get(this.buttonLogin).click();
+  }
+
+  digitarEmail(email) {
+    cy.get(this.inputEmail).type(email);
+  }
+
+  digitarSenha(senha) {
+    cy.get(this.inputSenha).type(senha);
+  }
+}
---
+++ cypress/support/steps/loginUsuario.step.js
@@ -0,0 +1,27 @@
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import { LoginUsuario } from "../pages/loginUsuario";
+Login = new LoginUsuario();
+
+Given("que acessei a página de login", function () {
+  cy.visit("/login");
+});
+
+When("coloco a minha senha", function () {
+  Login.digitarSenha("123456");
+});
+
+When("confirmo a operação", function () {
+  Login.clicarLogin();
+});
+
+When("não coloco a minha senha", function () {});
+
+When("digito a senha incorreta", function () {
+  Login.digitarSenha("12355555");
+});
+
+Then("coloco meu email", function () {
+  Login.digitarEmail("carolteste@email.com");
+});
+
+Then("eu não digito o email", function () {});
